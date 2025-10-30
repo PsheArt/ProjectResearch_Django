c3dd5d15de652bc2b90b0e12b5c8e4cdc409c9d3
Tema <pshenichnykh.art@yandex.com>
2025-10-30 18:47:29 +1200

    Update urls.py

--- research_project/accounts/urls.py
+++ research_project/accounts/urls.py
@@ -1,7 +1,6 @@
-from django.contrib import admin
+
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.views.generic.base import TemplateView
 from .views import register
 
 urlpatterns = [
